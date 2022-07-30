25f2d33d429a965e23a24a025bf1918301269ce9
Rovshana <rovshanarzayeva2001@gmail.com>
2022-07-30 12:24:08 +0400

    deleting some components

--- features/Home/HomeContainer.js
+++ features/Home/HomeContainer.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import HomeFirst from '../../shared/components/HomeComponent/HomeFirst'
-import Card from '../../shared/components/cards/Card'
+
 import Menu from '../../shared/components/Menu/Menu'
-import Footer from '../../shared/components/footer/Footer'
-import CardsBottom from '../../shared/components/cardsBottom/CardsBottom'
+
 
  function HomeContainer(props) {
     
@@ -12,10 +11,10 @@ import CardsBottom from '../../shared/components/cardsBottom/CardsBottom'
         <>
        <h3>Navbar will come there :)</h3>
         <HomeFirst/>
-        <Card />
+
         <Menu />
-        <CardsBottom />
-        <Footer/>
+        
+      
             
         </>
     )
