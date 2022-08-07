54e52817ee0ab8512a0c28098b5195a6b98f0b5b
Rovshana <rovshanarzayeva2001@gmail.com>
2022-08-07 15:48:21 +0400

    accordian has added

--- shared/components/Faqs/FAQs.js
+++ shared/components/Faqs/FAQs.js
@@ -8,19 +8,20 @@ function FAQs(props) {
     
 
     return (
-        <div className='container main'>
+        <div className='container'>
+          <h3 className='title'>F.A.Q</h3>
+          <div className='test'>
          <Accordion>
         <AccordionSummary
           addIcon={<AddIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Accordion 1</Typography>
+          <Typography>How to contact with Customer Service?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          Our Customer Experience Team is available 7 days a week and we offer 2 ways to get in contact.Email and Chat . We try to reply quickly, so you need not to wait too long for a response!.
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -30,24 +31,59 @@ function FAQs(props) {
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
-          <Typography>Accordion 2</Typography>
+          <Typography>I am completely alien to everything that is technology. Can I adopt Foody?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+           Foody is exactly for you, it has flexible structure, you can easily order whatever dishes you want.
           </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion disabled>
-     <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
+      
+      <Accordion>
+        <AccordionSummary
+          addIcon={<AddIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
         >
-          <Typography>Disabled Accordion</Typography>
-        </AccordionSummary> */
-       </Accordion>   
+          <Typography> How long is delivery time?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          Delivery time varies by location and by time of day. However, you will be notified when your order is ready or when it is picked up.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          addIcon={<AddIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>What Payment method can I use?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          For payment, we accept Credit Cards, PayPal and as well as you can pay at door.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          addIcon={<AddIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>What are your delivery Hours?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          Our delivery hour is from 10:00 AM  to 11PM.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      
+       </div> 
          </ div>
     )
 }
