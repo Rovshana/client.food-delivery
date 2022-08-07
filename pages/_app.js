39afc9e73d43352e414e92b2c9b430fd9ac06cc5
Rovshana <rovshanarzayeva2001@gmail.com>
2022-08-07 13:45:44 +0400

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
 import '../styles/login.css'
@@ -8,7 +8,6 @@ import '../styles/about.css'
 import '../styles/langbar.css'
 import '../styles/question.css'
 import '../styles/homefirst.scss'
-=======
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import "../styles/login.css";
@@ -18,21 +17,19 @@ import "../styles/about.css";
 import "../styles/how.css";
 import "../styles/langbar.css";
 import "../styles/homefirst.scss";
->>>>>>> 364d84f142d9eebc56856a8ad96a6424a0bfb0f9
+
 import "../i18n";
 import Layout from '../features/Layout/Layout'
 
-import { store } from "../store";
-import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Provider store={store}>
+   
           <Layout>
         <Component {...pageProps} />
          </Layout>
-      </Provider>
+   
     </>
   );
 }
