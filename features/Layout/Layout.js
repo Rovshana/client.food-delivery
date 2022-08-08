feb49a32e7200de913b42d7f46c194e95bc22beb
Metin1323 <95915073+Metin-Quliyev@users.noreply.github.com>
2022-08-08 16:54:01 +0400

    update login

--- features/Layout/Layout.js
+++ features/Layout/Layout.js
@@ -1,18 +1,16 @@
-import Navbar from "../../shared/components/Navbar/Navbar"
-import Footer from "../../shared/components/Footer/Footer"
-import {useRouter} from 'next/router'
-const Layout = ({children}) => {
-    const route = useRouter()
-    
-    return ( 
-        <div>
-            {route.pathname === "/" ? null : <Navbar/> }
-           
-            {children}
-            <Footer/>
-        </div>
+import Navbar from "../../shared/components/Navbar/Navbar";
+import Footer from "../../shared/components/Footer/Footer";
+import { useRouter } from "next/router";
+const Layout = ({ children }) => {
+  const route = useRouter();
 
-     )
-}
- 
+  return (
+    <div>
+      {route.pathname === "/login" ? null : route.pathname === "/" ? null : <Navbar />}
+      {children}
+     {route.pathname === "/login" ? null : <Footer />}
+    </div>
+  );
+};
+
 export default Layout;
--- pages/login.js
+++ pages/login.js
@@ -4,31 +4,30 @@ import { usersApi } from "../api/login";
 import LoginContainer from "../features/Login/LoginContainer";
 import { setUsers } from "../store/slices/LoginSlices";
 
-function Login({res}) {
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    dispatch(setUsers(res.Users))
-  })
+function Login({ res }) {
+ 
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setUsers(res.Users));
+  });
   return <LoginContainer />;
 }
 
 export async function getStaticProps() {
-  const res = await usersApi.then(res=>res.data)
+  const res = await usersApi.then((res) => res.data);
   if (res) {
     return {
-      props: {
+      props: {  
         res,
       },
-    }
-  }
-  else{
+    };
+  } else {
     return {
       props: {
-        res:"Gelmedi"
+        res: "Gelmedi",
       },
-    }
+    };
   }
 }
-
 
 export default Login;
