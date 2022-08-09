f8a4f8e4e1a7f447c86a7571fda440c2226533ff
GuliyevMatin <metinquliyev1323@gmail.com>
2022-08-10 00:48:59 +0400

    created pages

--- features/RestaurantPayment/RestaurantPayment.styled.js
+++ features/RestaurantPayment/RestaurantPayment.styled.js
@@ -1,35 +1,34 @@
-import styled from "styled-components"
+import styled from "styled-components";
+
+export const RestaurantPayment = styled.div`
+  /* background: red; */
+`;
+
+export const RestaurantImage = styled.div`
+  width: 100%;
+  height: 500px;
+  & img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+export const RestaurantInformation = styled.div`
+  background-color: red;
+`;
+
+export const RestaurantProductsContainer = styled.div``;
+
+export const RestaurantProducts = styled.div``;
+
+export const RestaurantProductsCounter = styled.div``;
 
 
-export const RestaurantPayment= styled.div`
+export const RestaurantInformationLeft = styled.div`
 
+`;
 
-`
+export const RestaurantInformationRight = styled.div`
 
-export const RestaurantImage= styled.div`
-
-
-`
-
-
-export const RestaurantInformation= styled.div`
-
-
-`
-
-
-export const RestaurantProductsContainer= styled.div`
-
-
-`
-
-
-export const RestaurantProducts= styled.div`
-
-
-`
-
-export const RestaurantProductsCounter= styled.div`
-
-
-`
+`;
