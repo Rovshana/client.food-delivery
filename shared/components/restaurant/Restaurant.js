7bdde448d8a062cf5aab6c3f1c0ea30348b4e5f7
Rovshana <rovshanarzayeva2001@gmail.com>
2022-08-10 12:39:26 +0400

    some changes

--- shared/components/restaurant/Restaurant.js
+++ shared/components/restaurant/Restaurant.js
@@ -1,11 +1,56 @@
 import React from 'react'
-
+import Image from 'next/image'
  function Restaurant(props) {
     
 
     return (
         <>
-        <h1>test</h1>
+        <div className='sidebar'>
+            <div className='parent2'>
+                <Image width='25' height='28' src='/restaurant/pizza.svg' />
+                <p className='type'>Chinese</p>
+            </div>
+            <div className='parent2'>
+                <Image width='25' height='28' src='/restaurant/pizza.svg' />
+                <p className='type'>Fast Food</p>
+            </div>
+            <div className='parent2'>
+                <Image width='25' height='28' src='/restaurant/pizza.svg' />
+                <p className='type'>Sea Food</p>
+            </div>
+            <div className='parent2'>
+                <Image width='25' height='28' src='/restaurant/pizza.svg' />
+                <p className='type'>Indian</p>
+            </div>
+            <div className='parent2'>
+                <Image width='25' height='28' src='/restaurant/pizza.svg' />
+                <p className='type'>Indian</p>
+            </div>
+            <div className='parent2'>
+                <Image width='25' height='28' src='/restaurant/pizza.svg' />
+                <p className='type'>Fast food</p>
+            </div>
+            <div className='parent2'>
+                <Image width='25' height='28' src='/restaurant/pizza.svg' />
+                <p className='type'>Indian</p>
+            </div>
+            <div className='parent2'>
+                <Image width='25' height='28' src='/restaurant/pizza.svg' />
+                <p className='type'>Kabab</p>
+            </div>
+            <div className='parent2'>
+                <Image width='25' height='28' src='/restaurant/pizza.svg' />
+                <p className='type'>Lahmacun</p>
+            </div>
+            <div className='parent2'>
+                <Image width='25' height='28' src='/restaurant/pizza.svg' />
+                <p className='type'>Salads</p>
+            </div>
+            <div className='parent2'>
+                <Image width='25' height='28' src='/restaurant/pizza.svg' />
+                <p className='type'>Indian</p>
+            </div>
+        </div>
             
         </>
     )
